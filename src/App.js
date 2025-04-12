4a8964a6be9afe170ace67efdfcb0e9ea03c8f1d
Sarah k <sarahkarboua16@gmail.com>
2025-04-12 18:51:38 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Timer from "./Timer";
+import AddTaskModal from "./AddTaskModal";
+import "./styles.css";
+  
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [showAddTaskModal, setShowAddTaskModal] = useState(false);
+
+  // Function to add a new task
+  const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+    setShowAddTaskModal(false);
+  };
+
+  // Function to remove a task
+  const handleRemoveTask = (taskToRemove) => {
+    setTasks(tasks.filter((task) => task !== taskToRemove));
+  };
+
+  return (
+    <div className="app">
+      {/* Header */}
+      <header>
+        <img src="/images/logo.png" alt="MentalHealth Logo" />
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Advice</li>
+            <li>Time Management</li>
+            <li>
+              <i className="fas fa-user"></i>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      {/* Timer Section */}
+      <section className="timer-section">
+        <Timer tasks={tasks} onRemoveTask={handleRemoveTask} />
+      </section>
+
+      {/* Add Task Button */}
+      <button className="add-task-button" onClick={() => setShowAddTaskModal(true)}>
+        <span className="circle-icon">+</span>
+        Add Task
+      </button>
+
+      {/* Add Task Modal */}
+      {showAddTaskModal && (
+        <AddTaskModal
+          onClose={() => setShowAddTaskModal(false)}
+          onAddTask={handleAddTask}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
